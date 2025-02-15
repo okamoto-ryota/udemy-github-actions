9aa0253df8d3a2727f3cb5d9491755d703f2df08
okamoto-ryota <okamoto-ryota@ksc.co.jp>
2025-02-15 19:28:22 +0900

    Revert "Update allowed cidr_ipv4 ec2"
    This reverts commit 5f163fffa488d6e72b847645941847447dc62454.

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,7 +101,7 @@ resource "aws_security_group" "ec2_main" {
 
 resource "aws_vpc_security_group_ingress_rule" "ec2_main_allow_ssh" {
   security_group_id = aws_security_group.ec2_main.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.allowed_ipv4_cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
